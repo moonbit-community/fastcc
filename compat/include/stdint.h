f49f9bb1bcd27428f7f48edeabb2cb65e16e8c6b
Wen Yuxiang <hackwaly@qq.com>
2026-01-15 14:35:09 +0800

    Implement mbtcc/selfhost fixes and compat headers

---
+++ compat/include/stdint.h
@@ -0,0 +1,35 @@
+#include <stddef.h>
+#ifndef _TCC_COMPAT_STDINT_H
+#define _TCC_COMPAT_STDINT_H
+
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+
+typedef short int16_t;
+typedef unsigned short uint16_t;
+
+typedef int int32_t;
+typedef unsigned int uint32_t;
+
+typedef long long int64_t;
+typedef unsigned long long uint64_t;
+
+typedef long intptr_t;
+typedef unsigned long uintptr_t;
+
+typedef long long intmax_t;
+typedef unsigned long long uintmax_t;
+
+#define INT8_C(c)   c
+#define INT16_C(c)  c
+#define INT32_C(c)  c
+#define INT64_C(c)  c##LL
+#define UINT8_C(c)  c##U
+#define UINT16_C(c) c##U
+#define UINT32_C(c) c##U
+#define UINT64_C(c) c##ULL
+
+#define INTMAX_C(c)  INT64_C(c)
+#define UINTMAX_C(c) UINT64_C(c)
+
+#endif /* _TCC_COMPAT_STDINT_H */
