505636f1eefaa557fb977e6a7ce08412cc63877b
Wen Yuxiang <hackwaly@qq.com>
2026-01-16 13:09:41 +0800

    bd sync: 2026-01-16 13:09:41

--- compat/include/libproc.h
+++
@@ -1,8 +0,0 @@
-#ifndef _TCC_COMPAT_LIBPROC_H
-#define _TCC_COMPAT_LIBPROC_H
-
-#include <stdint.h>
-
-int proc_pidpath(int pid, void *buffer, uint32_t buffersize);
-
-#endif /* _TCC_COMPAT_LIBPROC_H */
