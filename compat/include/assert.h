13ff90278b9a390ced209316892750b90897f44b
Wen Yuxiang <hackwaly@qq.com>
2026-01-16 12:45:51 +0800

    Restore tinycc port changes after bd sync

---
+++ compat/include/assert.h
@@ -0,0 +1,10 @@
+#ifndef _TCC_COMPAT_ASSERT_H
+#define _TCC_COMPAT_ASSERT_H
+
+#ifdef NDEBUG
+#define assert(x) ((void)0)
+#else
+#define assert(x) ((void)0)
+#endif
+
+#endif /* _TCC_COMPAT_ASSERT_H */
