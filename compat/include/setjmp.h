eb6559a842c703cd03f752ca30806b8339976fe0
Wen Yuxiang <hackwaly@qq.com>
2026-01-16 08:11:05 +0800

    bd sync: 2026-01-16 08:11:05

--- compat/include/setjmp.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TCC_COMPAT_SETJMP_H
-#define _TCC_COMPAT_SETJMP_H
-
-typedef long jmp_buf[32];
-
-int setjmp(jmp_buf env);
-void longjmp(jmp_buf env, int val);
-
-#endif /* _TCC_COMPAT_SETJMP_H */
